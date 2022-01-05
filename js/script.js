fcd12e6061b051f56b975a88617e5a3be6e70297
dohyun <tony7320@gmail.com>
2022-01-05 09:38:43 +0900

    navBar collapsible 변경 accordion 속성 false

--- js/script.js
+++ js/script.js
@@ -1,16 +1,18 @@
 // dropdown Init
 document.addEventListener('DOMContentLoaded', function () {
   let options = {
-    dropdownOptions: {
-      alignment: 'right',
-      hover: true,
+    collapsibleOptions: {
+      accordion: false,
     },
   };
   //   let dropDownElem = document.querySelectorAll('.dropdown-trigger');
   //   let dropDownInstance = M.Dropdown.init(dropDownElem, options);
-  let sideNavElem = document.querySelectorAll('.sidenav');
-  let sideNavInstance = M.Sidenav.init(sideNavElem, options);
+  // let sideNavElem = document.querySelectorAll('.sidenav');
+  // let sideNavInstance = M.Sidenav.init(sideNavElem, options);
   let collapsibleElem = document.querySelector('.collapsible');
-  let collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+  let collapsibleInstance = M.Collapsible.init(
+    collapsibleElem,
+    options.collapsibleOptions,
+  );
 });
 // dripdown Init
